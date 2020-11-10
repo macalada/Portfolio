4917d80863fa2d6e6dd92be42a15ea5aa32ccd8b
Yanti Sutrisno <wijayanti.sutrisno@live.com>
2020-11-10 15:35:02 -0700

    added user registration

--- blog/theblog/urls.py
+++ blog/theblog/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from . views import HomeView, PostDetailView, AddPostView
+from . views import HomeView, PostDetailView, AddPostView, UpdatePostView, DeletePostView
 
 urlpatterns = [
     # path('', views.home, name="home")
     path('', HomeView.as_view(), name="home"),
     path('post/<int:pk>', PostDetailView.as_view(), name="post_detail"),
-    path('add_post/', AddPostView.as_view(), name="add_post")
+    path('add_post/', AddPostView.as_view(), name="add_post"),
+    path('post/edit/<int:pk>',UpdatePostView.as_view(), name="update_post"),
+    path('post/<int:pk>/delete',DeletePostView.as_view(), name="delete_post"),
 ]
--- blog/theblog/views.py
+++ blog/theblog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
+from .forms import PostForm, UpdatePostForm
+from django.urls import reverse_lazy
+
 
 
 # def home(request):
@@ -9,6 +12,7 @@ from .models import Post
 class HomeView(ListView):
     model = Post
     template_name = "home.html"
+    ordering = ['-post_date']
 
 class PostDetailView(DetailView):
     model = Post
@@ -16,5 +20,18 @@ class PostDetailView(DetailView):
 
 class AddPostView(CreateView):
     model = Post
+    form_class = PostForm
     template_name = "add_post.html"
-    fields = '__all__'
+    # fields = '__all__'
+    # fields = ('title','author', 'body')
+
+class UpdatePostView(UpdateView):
+    model = Post
+    form_class = UpdatePostForm
+    template_name = "update_post.html"
+    # fields =['title', 'title_tag', 'body']
+
+class DeletePostView(DeleteView):
+    model = Post
+    template_name = "delete_post.html"
+    success_url= reverse_lazy('home')
